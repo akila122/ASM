903b7241234ec570f42331f8302ad417776b0a8d
Aleksa Rajković <aleksa.rajkovic@rbt.rs>
2021-01-05 19:49:51 +0100

    Update primary_parser

--- parsers/primary_parser.py
+++ parsers/primary_parser.py
@@ -7,9 +7,10 @@ from config import ATP_PLAYERS_CSV_URL, ATP_RANKINGS_CURRENT_CSV_URL, PLAYERS_FI
 
 @logger.catch(reraise=True)
 def parse_players() -> [Player]:
-    players_df = read_csv(ATP_PLAYERS_CSV_URL)
+    players_df = read_csv(ATP_PLAYERS_CSV_URL, header=None)
     logger.info(f'Fetched data from {ATP_PLAYERS_CSV_URL}')
-    current_rankings_df = read_csv(ATP_RANKINGS_CURRENT_CSV_URL)
+    cr_df = read_csv(ATP_RANKINGS_CURRENT_CSV_URL, header=None,
+                                   names=['date', 'rank', 'player_id', 'points'])
     logger.info(f'Fetched data from {ATP_RANKINGS_CURRENT_CSV_URL}')
     players = []
     for index, row in players_df.iterrows():
@@ -18,9 +19,9 @@ def parse_players() -> [Player]:
             first_name=row[PLAYERS_FIRST_NAME_INDEX],
             last_name=row[PLAYERS_LAST_NAME_INDEX],
             country_code=row[PLAYERS_COUNTRY_CODE_INDEX],
-            current_rank=-1
+            current_rank=int(cr_df[cr_df.player_id == row[PLAYERS_ID_INDEX]]['rank'] or -1)
         ))
-    return players
+        return players
 
 
 print(parse_players())
